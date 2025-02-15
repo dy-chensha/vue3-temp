e6f8b477b8875284770030372f7dc2b8873f8f99
邓宇 <dengyu@comac.cc>
2025-02-15 13:46:29 +0800

    代码格式化测试

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-    <div>
-
-    </div>
+    <div></div>
 </template>
 
 <script setup>
- for (let i = 0; i < 100; i++) {
-   console.log(i)
- }
+for (let i = 0; i < 100; i++) {
+
+    console.log(i);
+    
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>

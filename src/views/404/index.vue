06d4574564d3c93695240b37b93e41bc9594754b
邓宇 <dengyu@comac.cc>
2025-02-23 15:08:55 +0800

    style: 格式修复

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,13 +1,9 @@
 <template>
-  <div>
-    <h1>404页面</h1>
-  </div>
+    <div>
+        <h1>404页面</h1>
+    </div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,51 +1,45 @@
 <template>
-  <div>
-    <el-form :inline="true" :modal="ruleForm" >
-      <el-form-item prop="username" >
-        <el-input v-model="ruleForm.username" >
-        </el-input>
-      </el-form-item>
-      <el-form-item prop="password" >
-        <el-input v-model="ruleForm.password" >
-        </el-input>
-      </el-form-item>
-    </el-form>
-    <el-button @click="login" >
-      登录
-    </el-button>
-  </div>
+    <div>
+        <el-form :inline="true" :modal="ruleForm">
+            <el-form-item prop="username">
+                <el-input v-model="ruleForm.username"> </el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input v-model="ruleForm.password"> </el-input>
+            </el-form-item>
+        </el-form>
+        <el-button @click="login"> 登录 </el-button>
+    </div>
 </template>
 
 <script setup>
-  import {reactive} from 'vue'
-  import useUserStore from "@/store/module/user.ts"
-  import {useRouter} from "vue-router"
-  import {ElMessage} from "element-plus"
-  const ruleForm = reactive({
-    username:'',
-    password:''
-  })
-  let userStore =useUserStore()
-  let $router = useRouter()
-  console.log($router);
-  const login = async ()=>{
+import { reactive } from 'vue';
+import useUserStore from '@/store/module/user.ts';
+import { useRouter } from 'vue-router';
+import { ElMessage } from 'element-plus';
+const ruleForm = reactive({
+    username: '',
+    password: '',
+});
+let userStore = useUserStore();
+let $router = useRouter();
+console.log($router);
+const login = async () => {
     try {
-      let res = await userStore.userLogin(ruleForm)
-      $router.push('/')
-      ElMessage({
-        type:'success',
-        message:'登陆成功'
-      })
+        let res = await userStore.userLogin(ruleForm);
+        $router.push('/');
+        ElMessage({
+            type: 'success',
+            message: '登陆成功',
+        });
     } catch (error) {
-      console.log(error);
-      ElMessage({
-        type:'error',
-        message:'账号或者密码错误'
-      })
+        console.log(error);
+        ElMessage({
+            type: 'error',
+            message: '账号或者密码错误',
+        });
     }
-  }
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
